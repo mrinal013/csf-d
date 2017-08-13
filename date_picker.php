278beaa5a3ebaaf20219f6613fac9c3e5320ee89
mrinal013 <mrinalhaque99@gmail.com>
2017-08-13 13:35:36 +0600

    first commit

---
+++ date_picker.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+/**
+ *
+ * Field: Datepicker
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ */
+class CSFramework_Option_date_picker extends CSFramework_Options {
+
+  public function __construct( $field, $value = '', $unique = '' ) {
+    parent::__construct( $field, $value, $unique );
+  }
+
+  public function output() {
+
+    echo $this->element_before();
+    echo '<input type="text" id="datepicker">';
+    echo $this->element_after();
+
+  }
+
+}
+
+if ( ! function_exists( 'admin_scripts' ) ) {
+	/**
+	 * Load jquery ui datepicker script and stylesheet.
+	 */
+	function admin_scripts() {
+		wp_enqueue_style( 'jquery-ui-css', '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
+		wp_enqueue_script('jquery-ui', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', array('jquery'));
+	}
+} // endif function_exists( 'admin_scripts' ).
+
+add_action( 'admin_enqueue_scripts', 'admin_scripts' );
