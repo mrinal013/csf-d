6bbe711a45e6c207e12fdabfc1043b990133445c
Mrinal Haque <mrinalhaque99@gmail.com>
2018-03-15 22:39:52 +0600

    Update date_picker.php
    issue: script miss - fixed

--- date_picker.php
+++ date_picker.php
@@ -23,6 +23,7 @@ class CSFramework_Option_date_picker extends CSFramework_Options {
 
 }
 
+add_action( 'admin_enqueue_scripts', 'admin_scripts' );
 if ( ! function_exists( 'admin_scripts' ) ) {
 	/**
 	 * Load jquery ui datepicker script and stylesheet.
@@ -31,6 +32,9 @@ if ( ! function_exists( 'admin_scripts' ) ) {
 		wp_enqueue_style( 'jquery-ui-css', '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css' );
 		wp_enqueue_script('jquery-ui', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', array('jquery'));
 	}
-} // endif function_exists( 'admin_scripts' ).
+} // endif function_exists( 'admin_scripts' )
 
-add_action( 'admin_enqueue_scripts', 'admin_scripts' );
+add_action('admin_footer', 'my_admin_add_js');
+function my_admin_add_js() {
+  echo '<script>$( "#datepicker" ).datepicker();</script>';
+}
